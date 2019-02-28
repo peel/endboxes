1556285a4e87679546f5683b163a16b782a691e8
Piotr Limanowski <piotr@codearsonist.com>
2019-02-28 20:24:57 +0100

    :recycle: make it more obvious what a project is

--- lorem-project/verify.el
+++ lorem-project/verify.el
@@ -1,6 +1,6 @@
 (progn 
   (let ((tmp-buffer (generate-new-buffer "sample")))
     (switch-to-buffer tmp-buffer))
-  (mesasage (concat "Sandbox: " (nix-current-sandbox)))
+  (message (concat "Sandbox: " (nix-current-sandbox)))
   (message (concat "From sandbox: " (shell-command-to-string "hello")))
   (switch-to-buffer-other-window "*Messages*"))
